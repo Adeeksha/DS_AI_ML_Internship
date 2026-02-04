13f248484a7f7c4bcbee92bf036f39ff55846117
Adeeksha.H.K <160483519+Adeeksha@users.noreply.github.com>
2026-02-04 12:43:00 +0530

    Add files via upload

---
+++ src/Day3/Day3_task1.py
@@ -0,0 +1,8 @@
+inventory=["Apples","Bananas","Dates","Carrots"]
+print("List:",inventory)
+inventory.append("Eggs")
+print("append:",inventory)
+inventory.remove("Bananas")
+print("remove:",inventory)
+inventory.sort()
+print("sort:",inventory)
