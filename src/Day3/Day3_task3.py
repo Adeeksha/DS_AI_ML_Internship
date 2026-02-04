242985afda6f95e76390328c4f2aed619f7b7db2
Adeeksha.H.K <160483519+Adeeksha@users.noreply.github.com>
2026-02-04 12:43:47 +0530

    Add files via upload

---
+++ src/Day3/Day3_task3.py
@@ -0,0 +1,6 @@
+screen_res=(1920,1080)
+print("Screen_resolution:",screen_res)
+print(f"current_resolution:{screen_res[0]}X{screen_res[1]}")
+#screen_res[0] = 1280
+print("Tuples cannot be modified")
+screen_res[0] = 1280
