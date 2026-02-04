983509aac62f82feee46ac6eeb732dfb5ea23a7b
Adeeksha.H.K <160483519+Adeeksha@users.noreply.github.com>
2026-02-04 12:43:21 +0530

    Add files via upload

---
+++ src/Day3/Day3_task2.py
@@ -0,0 +1,5 @@
+temparatures=[22,24,25,28,30,29,27,26,24,22]
+print("First_Reading:",temparatures[0])
+print("Last_Reading:",temparatures[-1])
+print("AFternoon_Peak:",temparatures[3:6])
+print("Last_3hours:",temparatures[-3:])
